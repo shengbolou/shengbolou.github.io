9fbbf812de0c5fa69b3aeeea7c57615d5c486093
shengbolou <loushengbo@hotmail.com>
2016-02-04 22:44:24 -0500

    add

--- js/sbl.js
+++ js/sbl.js
@@ -3,6 +3,7 @@ $(document).ready(function(){
   var check = 0;
 
   $('.pageheader').css('height',myHeight+30);
+  $('.downward').css('top',myHeight-50);
   $(".navbar-toggle").on("click", function () {
       $(this).toggleClass("active");
   });
@@ -14,10 +15,19 @@ $(document).ready(function(){
        check=1;
     }
     else if($(this).scrollTop() == 0){
+      //scroll to top
       $('.navbar').velocity('transition.slideUpOut',200);
       check=0;
     }
   });
 
   $('.myheader').velocity('transition.slideRightIn',{delay:2500});
+
+  $('.downward').velocity({
+    translateY:'10px'
+  },{loop:true}).velocity('reverse');
+
+  $('.downward').click(function(){
+    $('.mine').velocity("scroll", { duration: 1500, easing: "spring" })
+  });
 });
