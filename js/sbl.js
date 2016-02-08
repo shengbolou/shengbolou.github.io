165b9f0308e122ce1e6f1daf5fbfa902df72ee71
shengbolou <loushengbo@hotmail.com>
2016-02-07 23:58:01 -0500

    add

--- js/sbl.js
+++ js/sbl.js
@@ -9,7 +9,60 @@ $(document).ready(function(){
       $(this).toggleClass("active");
   });
 
+  $('.hover').velocity({backgroundColor: '#5C5959' ,backgroundColorAlpha: 0.6});
+  $('.hover3').velocity({backgroundColor: 'rgb(255, 255, 255)' ,backgroundColorAlpha: 0.6});
+  $('.naptime').hover(
+    function(){
+      $(this).find('.hover').velocity('transition.slideUpIn',500);
+       $(this).velocity({
+         scale:'1.1'
+       });
+    },function(){
+      $(this).find('.hover').velocity('transition.slideDownOut',500);
+      $(this).velocity({
+        scale:'1'
+      });
+  });
 
+$('.hover2').css('transform','scale(0)');
+  $('.github').hover(
+    function(){
+      $(this).find('.hover2').velocity({
+        scale:'1.1',
+      },"spring",1000);
+       $(this).velocity({
+         scale:'1.1',
+         boxShadowBlur: 20
+       },"spring",500);
+    },function(){
+      $(this).find('.hover2').velocity({
+        scale:'0',
+      },"spring",1000);
+       $(this).velocity({
+         scale:'1',
+         boxShadowBlur: 0
+       },"spring",500);
+  });
+
+ $('.ani').css('width','0');
+  $('.more').hover(
+    function(){
+      $(this).find('.hover3').velocity('transition.slideLeftIn',500);
+      $(this).find('.ani').velocity({delay:1000}).velocity({
+        width:'400px'
+      });
+       $(this).velocity({
+         scale:'1.1'
+       });
+    },function(){
+      $(this).find('.hover3').velocity('transition.slideLeftOut',500);
+      $(this).velocity({
+        scale:'1'
+      });
+      $(this).find('.ani').velocity({delay:1000}).velocity({
+        width:'0px'
+      });
+  });
 
   $(window).scroll(function(){
     if($(this).scrollTop() >= 150 && check==0){
